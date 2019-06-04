d43daa05ee3a6abcbab90771f2583c1520ca0f12
Ellen Marie Dash <me@duckie.co>
2019-06-04 17:03:28 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Timer
+
+This is a basic stopwatch/timer tool made using Godot.
+
+Controls:
+
+- spacebar = start/stop
+- escape = reset the timer, if it's stopped
---
+++ src/Label.gd
@@ -0,0 +1,54 @@
+extends Label
+
+onready var timer = $Timer
+var mins
+var secs
+
+var running = false
+
+func _ready():
+	redraw(true)
+	timer.wait_time = 1
+	timer.one_shot = false
+	timer.connect("timeout", self, "timeout_handler")
+
+func _process(delta):
+	if not running and Input.is_action_just_pressed("ui_cancel"):
+		stop()
+		redraw(true)
+		return
+	
+	if not Input.is_action_just_pressed("ui_accept"):
+		return
+	
+	if running:
+		stop()
+	else:
+		start()
+
+func start():
+	mins = 0
+	secs = 0
+	redraw()
+	timer.start()
+	running = true
+
+func stop():
+	timer.stop()
+	running = false
+
+func redraw(reset=false):
+	if reset:
+		self.text = "--:--"
+	else:
+		var display_mins = str(mins).pad_zeros(2)
+		var display_secs = str(secs).pad_zeros(2)
+		self.text = display_mins + ":" + display_secs
+
+func timeout_handler():
+	secs += 1
+	if secs > 59:
+		mins += 1
+		secs = 0
+	
+	redraw()
